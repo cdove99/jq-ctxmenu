815942dccb3f7c87952f70b9e1bb61c158cf5b53
ZachW <zach@hertzian.co.uk>
2020-01-07 10:37:52 +0000

    First version of menu plugin

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # jq-ctxmenu
 
 jQuery context menu / dropdown menus  
-Basically made a jQuery plugin to create some menus. Just because.  
+Basically made a jQuery plugin to create some right-click menus.  
 
 ## Usage  
 
-Initialise a dropdown with `$('.selector').ctxmenu(options);`  
-`options` can be a string or object, depending on purpose.  
+Initialise a dropdown with: `$('.selector').ctxmenu(options);`  
+`options` should be an object.  
+
+Example:  
+
+```javascript
+    $('#menu').ctxmenu({
+        class: 'mymenu',
+        items: [
+            {
+                name: 'About',
+                callback: function() {
+                    alert('This is an alert dialog!');
+                },
+                class: 'about-item',
+            }
+        ],
+    });
+```  
+
+The menu should adjust on open in case it falls out of the window.  
 
 ## Options object  
 
@@ -15,11 +34,8 @@ Keys can include:
 
 - `items`: *IMPORTANT* An Array of objects to use as menu items. Each object MUST contain the keys `name` and `callback`. `name` will be displayed in menu, `callback` is an event that fires when the option is selected. `class` can also be given, and this is applied to the `span` element which holds the `name` string on display. Using this, you can apply special effects or displays to individual menu items, such as icons within the `:before / :after` pseudo element(s).  
 - `class`: *Optional* String class name to apply to the menu for optional additional styling.  
-- `location`: *default: event* String to determine what the menu is appearing relative to. Either `event` (defaults to this if not given), or `element` to place it relative to the element.
+- `loc`: *default: "element"* Either an `Event` object, or a String as  `"element"` (defaults to this if not given) to place it relative to the element.
 
-## Option strings
+## Future Ideas/Improvements  
 
-The current option strings that can be used are:  
-
-- `destroy`: Remove the menu for the given element.
-- `getitems`: Get an Array containing the String named items of the list.
+- Nested lists/options?
--- src/js/jq-ctxmenu.js
+++ src/js/jq-ctxmenu.js
@@ -1,47 +1,131 @@
 /**
  * jQuery ctxmenu plugin.
  * Create dropdown menus with a simple initialisation!
+ * Selected elements should be relatively positioned.
  */
+var jqctxDefaults = {
+    menuid: 'jq-ctx-menu-id',
+    menucls: 'jq-ctx-menu-parent',
+    itemcls: 'jq-ctx-menu-item',
+};
 
 (function($) {
-    var defaults = {
-        menucls: '',
-        itemcls: '',
-    };
-
     // functions to assist
     var fn = {
         // misc
-        warn: function(msg) {
-            if (!msg) return;
-            console.warn('JQ.ctxmenu: ' + msg);
-        },
         bind: function($el, evt, fn) { $el.on(evt, fn); },
-        open: function() {},
         // helpers
-        destroy: function(elem) {},
-        getItems: function(elem) {},
-        init: function(elem, options) {},
+        init: function(elem, options) {
+            // Create menu elem
+            if (!Array.isArray(options.items)) 
+                throw Error("No items given for this menu.");
+            // find or create menu elem
+            var $menu = $('#'+jqctxDefaults.menuid);
+            if ($menu.length === 0) {
+                $menu = fn.menu(options);
+                $menu.hide();
+                $("body").append($menu);
+                $('body').on('click', function(evt) {
+                    if (!$(evt.target).hasClass(jqctxDefaults.itemcls)) {
+                        $menu.hide();
+                    }
+                });
+            }
+            $menu.find("ul").empty();
+            // items
+            for (var i=0; i<options.items.length; i++) {
+                var item = options.items[i];
+                $menu.find("ul").append(fn.item(
+                    item.name,
+                    item.callback,
+                    item.class
+                ));
+            }
+            // open
+            fn.open(elem, $menu, options);
+        },
+        open: function(elem, $menu, options) {
+            // var
+            var pos = {top: 0, left: 0};
+            var elemdim = null;  // Element h/w
+            var _type;
+            // Position math begins
+            if (options.loc instanceof $.Event) {
+                _type = 'evt';
+                pos.left = options.loc.pageX;
+                pos.top = options.loc.pageY;
+            } else if (options.loc === "element" || !options.loc) {
+                _type = 'elem';
+                var offset = $(elem).offset();
+                elemdim = {
+                    height: $(elem).outerHeight(),
+                    width: $(elem).outerWidth(),
+                };
+                pos = Object.assign(offset);
+                pos.top += elemdim.height;
+            }
+            // Adjust for screen - vertical
+            var _viewBottom = $(window).scrollTop() + $(window).height();
+            var _bottom = pos.top + $menu.outerHeight();
+            if (_bottom > _viewBottom) {  // off screen, flip
+                pos.top -= $menu.outerHeight();
+                if (_type === "elem") pos.top -= elemdim.height;
+            }
+            // Adjust for screen - horizontal
+            var _viewLeft = $(window).scrollLeft() + $(window).width();
+            var _left = pos.left + $menu.outerWidth();
+            if (_left > _viewLeft) {  // off screen, flip
+                pos.left -= $menu.outerWidth();
+                if (_type === "elem") pos.left -= elemdim.width;
+            }
+            // apply
+            $menu.css(pos).show();
+        },
+        close: function() {
+            $('#'+jqctxDefaults.menuid).hide();
+        },
         // elem construction
-        menu: function(items) {},
-        item: function(value, callback, cls) {},
+        menu: function(options) {
+            var $menu = $("<div></div>");
+            $menu.addClass(jqctxDefaults.menucls)
+            .attr('id',jqctxDefaults.menuid);
+            if (options.class) $menu.addClass(options.class);
+            // Option list
+            $menu.append("<ul></ul>");
+            return $menu;
+        },
+        item: function(value, callback, cls) {
+            if (!callback || !value) return;  // Cancel/ignore
+            // create
+            var $item = $("<li></li>");
+            $item.addClass(jqctxDefaults.itemcls);
+            if (cls) $item.addClass(cls);
+            // value
+            $item.append("<span>" + value.toString() + "</span>");
+            // event
+            var callAndClose = function() {
+                var complete = callback();
+                // Close menu if no return/truthy
+                if (complete || complete === undefined) 
+                    fn.close();
+            };
+            fn.bind($item, "click", callAndClose);
+            return $item;
+        },
     };
 
-    $.fn.ctxmenu = function(options) {
-        return this.each(function() {
-            if (typeof options === "object" && !Array.isArray(options)) {
-                fn.init(this, options);  // init
-            } else {
-                switch (options) {
-                    case 'destroy':
-                        fn.destroy(this);
-                        break;
-                    case 'getitems':
-                        fn.getItems(this);
-                    default:
-                        fn.warn("Unrecognised action, ignoring.");
-                }
+    $.fn.ctxmenu = function(options) {  
+        if (typeof options === "object" && !Array.isArray(options)) {
+            // init
+            return this.each(function() { fn.init(this, options); });
+        } else {
+            switch (options) {
+                case 'destroy':
+                    return this.each(function() { fn.destroy(this); });
+                default:
+                    console.warn('Unrecognised action, ignoring.');
+                    return this;
             }
-        });
+        }
     };
 })(jQuery);
